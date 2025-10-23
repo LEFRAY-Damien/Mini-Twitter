0d7d68d1139b7f8a4d18b8ce72b7c8f7a24d5cf0
Kyllian <kyllianlerousseau85@orange.fr>
2025-10-23 10:20:00 +0200

    modif css + ajout contraintes

--- src/Form/RepliesType.php
+++ src/Form/RepliesType.php
@@ -10,6 +10,8 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Regex;
+use Symfony\Component\Validator\Constraints as Assert;
 
 class RepliesType extends AbstractType
 {
@@ -17,7 +19,9 @@ class RepliesType extends AbstractType
     {
         $builder
             ->add(
-                'content',TextareaType::class,[
+                'content',
+                TextareaType::class,
+                [
                     'label' => false,
                     'attr' => [
                         'placeholder' => 'Postez votre réponse',
@@ -26,6 +30,15 @@ class RepliesType extends AbstractType
                         'rows' => 4,
                         'cols' => 60,
                         'class' => 'text-white border border-white rounded-lg p-3 bg-[#15202B]'
+                    ],
+                    'constraints' => [
+                        new Regex([
+                            'pattern' => '/^[\p{L}\p{N}\s_\-.,!?()\'"]+$/u',
+                            'message' => 'La réponse ne peut contenir seulement des lettres, chiffres ou underscores.'
+                        ]),
+                        new Assert\NotBlank([
+                            'message' => 'La réponse ne peut pas être vide.'
+                        ])
                     ]
                 ]
             );
--- src/Form/ReportType.php
+++ src/Form/ReportType.php
@@ -7,16 +7,37 @@ use App\Entity\Tweet;
 use App\Entity\User;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Regex;
+use Symfony\Component\Validator\Constraints as Assert;
 
 class ReportType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('reason')
-            ;
+            ->add('reason', TextareaType::class, [
+                'label' => false,
+                'attr' => [
+                    'placeholder' => 'Expliquez nous la raison de ce signalement.',
+                    'maxlength' => 280,
+                    'style' => 'resize: none;',
+                    'rows' => 4,
+                    'cols' => 60,
+                    'class' => 'text-white border border-white rounded-lg p-3 bg-[#15202B]'
+                ],
+                'constraints' => [
+                    new Regex([
+                        'pattern' => '/^[\p{L}\p{N}\s_\-.,!?()\'"]+$/u',
+                        'message' => 'Le signalement ne peut contenir seulement des lettres, chiffres ou underscores.'
+                    ]),
+                    new Assert\NotBlank([
+                        'message' => 'Le signalement ne peut pas être vide.'
+                    ])
+                ]
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
